db77e785e1adc82b0825a4808f93ff38ba2980cd
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2024-07-09 07:35:15 +0800

    Update CreditsVideo.hx

--- source/CreditsVideo.hx
+++ source/CreditsVideo.hx
@@ -4,11 +4,24 @@ import flixel.FlxG;
 import flixel.FlxState;
 import flixel.util.FlxColor;
 
+
+#if VIDEOS_ALLOWED
+#if (hxCodec >= "3.0.0")
+import hxcodec.flixel.FlxVideo as MP4Handler;
+#elseif (hxCodec == "2.6.1")
+import hxcodec.VideoHandler as MP4Handler;
+#elseif (hxCodec == "2.6.0")
+import VideoHandler as MP4Handler;
+#else
+import vlc.MP4Handler;
+#end
+#end
+
+
 using StringTools;
 
 class CreditsVideo extends FlxState
 {
-	var oldFPS:Int = VideoHandler.MAX_FPS;
 	var video:VideoHandler;
 	var titleState = new TitleState();
 
@@ -16,24 +29,8 @@ class CreditsVideo extends FlxState
 	{
 
 		super.create();
-
-		VideoHandler.MAX_FPS = 60;
-
-		video = new VideoHandler();
-
-		video.playMP4(Paths.video('credits'), function(){
-			next();
-			#if web
-				VideoHandler.MAX_FPS = oldFPS;
-			#end
-		}, false, false);
-
-		video.width = 1280;
-		video.height = 720;
-		video.updateHitbox();
-		video.setPosition(0,0);
-
-		add(video);
+	        
+	
 	}
 
 	override public function update(elapsed:Float){
@@ -45,5 +42,44 @@ class CreditsVideo extends FlxState
 	function next():Void{
 		FlxG.switchState(titleState);
 	}
+
+        public function startVideo(name:String)
+	{
+		#if VIDEOS_ALLOWED
+		var filepath:String = Paths.video(name);
+		#if sys
+		if(!FileSystem.exists(filepath))
+		#else
+		if(!OpenFlAssets.exists(filepath))
+		#end
+		{
+			FlxG.log.warn('Couldnt find video file: ' + name);
+			next();
+			return;
+		}
+
+		var video:MP4Handler = new MP4Handler();
+		#if (hxCodec < "3.0.0")
+		video.playVideo(filepath);
+		video.finishCallback = function()
+		{
+			next();
+			return;
+		}
+		#else
+		video.play(filepath);
+		video.onEndReached.add(function(){
+			video.dispose();
+			next();
+			return;
+		});
+		#end
+		#else
+		FlxG.log.warn('Platform not supported!');
+		next();
+		return;
+		#end
+	}
+
 	
 }
