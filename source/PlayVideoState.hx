49f8beb956c09c0aa59d18eff66f2d98a1381530
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2024-07-09 12:53:56 +0800

    Update PlayVideoState.hx

--- source/PlayVideoState.hx
+++ source/PlayVideoState.hx
@@ -22,6 +22,18 @@ using StringTools;
 import sys.FileSystem;
 #end
 
+#if VIDEOS_ALLOWED
+#if (hxCodec >= "3.0.0")
+import hxcodec.flixel.FlxVideo as MP4Handler;
+#elseif (hxCodec == "2.6.1")
+import hxcodec.VideoHandler as MP4Handler;
+#elseif (hxCodec == "2.6.0")
+import VideoHandler as MP4Handler;
+#else
+import vlc.MP4Handler;
+#end
+#end
+
 class PlayVideoState extends MusicBeatState
 {
 
